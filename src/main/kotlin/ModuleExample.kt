3c0334af23997db4ef66d93e0524b16aead237ba
ItCameFr0mMars <48456413+ItCameFr0mMars@users.noreply.github.com>
2021-11-05 13:01:19 -0400

    Update ModuleExample.kt

--- src/main/kotlin/ModuleExample.kt
+++ src/main/kotlin/ModuleExample.kt
@@ -8,63 +8,30 @@ import net.minecraft.entity.Entity
 import net.minecraft.network.play.client.CPacketUseEntity
 import net.minecraft.util.EnumHand
 
-internal object ModuleExample: PluginModule(
-    name = "ModuleExample",
-    category = Category.MISC,
-    description = "Example module which mounts entities using packets",
+internal object GridWalk: PluginModule(
+    name = "GridWalk",
+    category = Category.MOVEMENT,
+    description = "annoying",
     pluginMain = PluginExample
 ) {
-    private val maxReach by setting("Max Reach", 4.9f, 0.0f..8.0f, 0.1f, description = "Player's Max Reach")
-    private val mountEntity = setting("Mount Entity", false, description = "Mounts the saved entity or falls back to the closest one")
-    private val saveEntity = setting("Save Entity", false, description = "Saves the entity to mount that intersects with player view")
-
-    private var entityToMount: Entity? = null
-
-    init {
-        saveEntity.consumers.add {_, it ->
-            if (it) {
-                mc.objectMouseOver?.entityHit?.let {
-                    entityToMount = it
-                    sendChatMessage("$chatName Entity saved ${it.positionVector} ID: ${it.entityId}")
-                } ?: run {
-                    sendChatMessage("$chatName No Entity was found at your current cursor position!")
-                }
-            }
-            false
-        }
-
-        mountEntity.consumers.add { _, it ->
-            if (it) {
-                runBlocking {
-                    entityToMount?.let {
-                        mc.connection?.sendPacket(CPacketUseEntity(it, EnumHand.MAIN_HAND))
-                    } ?: run {
-                        sendChatMessage("No entity was saved, falling back to the closest entity.")
-                        onMainThreadSafe {
-                            mountClosestEntity()
-                        }
-                    }
-                }
-            }
-            false
-        }
+    private enum class AutoWalkMode(override val displayName: String) : DisplayEnum {
+    FORWARD("Forward"),
+    BACKWARD("Backward")
     }
+    init {
+        listener<ConnectionEvent.Disconnect> {
+            if (disableOnDisconnect) disable()
+                listener<InputUpdateEvent>(6969) {
+            if (LagNotifier.paused && LagNotifier.pauseAutoWalk) return@listener
 
-    private fun SafeClientEvent.mountClosestEntity() {
-        world.loadedEntityList
-            .filter {
-                it != player.ridingEntity
-            }.minByOrNull {
-                player.getDistanceSq(it)
-            } ?.let {
-                if (maxReach == 0.0f || player.getDistance(it) <= maxReach) {
-                    sendChatMessage("$chatName Mounting: ${it.name}@${it.positionVector}")
-                    connection.sendPacket(CPacketUseEntity(it, EnumHand.MAIN_HAND))
-                } else {
-                    sendChatMessage("$chatName Closest entity too far away: ${it.name}@${it.positionVector.distanceTo(player.positionVector)}")
+            if (it.movementInput !is MovementInputFromOptions) return@listener
+
+            when (mode.value.displayName) {
+                AutoWalkMode.FORWARD -> {
+                    it.movementInput.moveForward = 1.0f
+                    }    
                 }
-            } ?: run {
-                sendChatMessage("$chatName Can't find any Entity in world.")
             }
+        }    
     }
 }
