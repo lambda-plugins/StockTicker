58f847d5af1cb47877f005ae783ce7c34b0fa00d
ItCameFr0mMars <48456413+ItCameFr0mMars@users.noreply.github.com>
2021-11-05 13:09:08 -0400

    Update PluginExample.kt

--- src/main/kotlin/PluginExample.kt
+++ src/main/kotlin/PluginExample.kt
@@ -4,7 +4,16 @@ internal object PluginExample: Plugin() {
 
     override fun onLoad() {
         // Load any modules, commands, or HUD elements here
-        modules.add(ModuleExample)
+        modules.add(com.lambda.client.util.TickTimer)
+        modules.add(com.lambda.client.event.SafeClientEvent)
+        modules.add(com.lambda.client.event.events.ConnectionEvent)
+        modules.add(com.lambda.client.util.threads.runSafe)
+        modules.add(com.lambda.client.util.threads.safeListener)
+        modules.add(com.lambda.commons.interfaces.DisplayEnum)
+        modules.add(com.lambda.event.listener.listener)
+        modules.add(net.minecraft.util.MovementInputFromOptions)
+        modules.add(net.minecraftforge.client.event.InputUpdateEvent)
+        modules.add(net.minecraftforge.fml.common.gameevent.TickEvent)
         commands.add(CommandExample)
         hudElements.add(LabelHudExample)
     }
