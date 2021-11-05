796c7cd59a7a0ac4d7ea97e212722653a7fa833f
ItCameFr0mMars <48456413+ItCameFr0mMars@users.noreply.github.com>
2021-11-05 14:04:09 -0400

    Update ModuleExample.kt

--- src/main/kotlin/ModuleExample.kt
+++ src/main/kotlin/ModuleExample.kt
@@ -1,12 +1,23 @@
+import baritone.api.pathing.goals.GoalXZ
 import com.lambda.client.event.SafeClientEvent
+import com.lambda.client.event.events.BaritoneCommandEvent
+import com.lambda.client.event.events.ConnectionEvent
 import com.lambda.client.module.Category
-import com.lambda.client.plugin.api.PluginModule
-import com.lambda.client.util.text.MessageSendHelper.sendChatMessage
-import com.lambda.client.util.threads.onMainThreadSafe
-import kotlinx.coroutines.runBlocking
-import net.minecraft.entity.Entity
-import net.minecraft.network.play.client.CPacketUseEntity
-import net.minecraft.util.EnumHand
+import com.lambda.client.module.Module
+import com.lambda.client.module.modules.player.LagNotifier
+import com.lambda.client.util.BaritoneUtils
+import com.lambda.client.util.TickTimer
+import com.lambda.client.util.TimeUnit
+import com.lambda.client.util.math.Direction
+import com.lambda.client.util.text.MessageSendHelper
+import com.lambda.client.util.threads.runSafe
+import com.lambda.client.util.threads.safeListener
+import com.lambda.commons.extension.floorToInt
+import com.lambda.commons.interfaces.DisplayEnum
+import com.lambda.event.listener.listener
+import net.minecraft.util.MovementInputFromOptions
+import net.minecraftforge.client.event.InputUpdateEvent
+import net.minecraftforge.fml.common.gameevent.TickEvent
 
 internal object ModuleExample: PluginModule(
     name = "PluginModule",
